4571390deefb4cd3b0f87d31e58a42edd1e89246
Mohamed Meabed <mo.meabed@gmail.com>
2018-09-19 04:43:29 +0400

    Update

--- server/main.go
+++ server/main.go
@@ -5,28 +5,63 @@ import (
 	"net/http"
 )
 
-var allowedExchanger = map[string]string{
-	`google`: `googleApi`,
-	`yahoo`:  `yahooApi`,
-	`fixer`:  `fixer`,
+var (
+	allowedExchanger = map[string]string{
+		`google`: `googleApi`,
+		`yahoo`:  `yahooApi`,
+		`fixer`:  `fixer`,
+	}
+
+	routes = map[string]func(w http.ResponseWriter, r *http.Request){
+		`/convert`: convert,
+		`/`:      etc,
+	}
+)
+
+var etc = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("etc")
+}
+
+var convert = func(w http.ResponseWriter, r *http.Request) {
+	payload := `{
+  "amount": 1,
+  "exchanger": [
+    {
+      "name": "google",
+      "userAgent": "firefox"
+    },
+    {
+      "name": "yahoo",
+      "userAgent": "Chrome"
+    },
+    {
+      "name": "currencyLayer",
+      "apiKey": "12312",
+      "userAgent": "currencyLayer Chrome"
+    },
+    {
+      "name": "fixer",
+      "apiKey": "12312",
+      "userAgent": "currencyLayer fixer"
+    }
+  ],
+  "from": "USD",
+  "to": "AED"
+}`
+	fmt.Println(payload)
+
 }
 
 func main() {
 
-	http.HandleFunc(`/convert`, convertOne)
-	http.HandleFunc(`/convert-multi`, convertMultiWithSwap)
+	// handle routers
+	for k, v := range routes {
+		http.HandleFunc(k, v)
+	}
 
 	fmt.Println(`server started`)
 	// todo
 	// port and config
 	// cache
 	http.ListenAndServe(":3003", nil)
-}
-
-var convertOne = func(w http.ResponseWriter, r *http.Request) {
-
-}
-
-var convertMultiWithSwap = func(w http.ResponseWriter, r *http.Request) {
-
 }
