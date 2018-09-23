2257f6fc31ac400ff3d7f0487fd84c823e72df6a
Mohamed Meabed <mo.meabed@gmail.com>
2018-09-23 19:13:01 +0400

    Update scripts

---
+++ scripts/server-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+my_dir="$(dirname "$0")"
+
+docker pull meio/go-swap-server:latest
+docker run --rm --name go-swap-server -u 0 -p 5000:5000 -it meio/go-swap-server:latest
