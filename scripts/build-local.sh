8b2dfe31cda67229af30ecbac5e9c1d97276a612
Mohamed Meabed <mo.meabed@gmail.com>
2018-09-25 00:53:15 +0400

    Update

--- scripts/build-local.sh
+++ scripts/build-local.sh
@@ -4,8 +4,5 @@ export CGO_ENABLED=0
 my_dir="$(dirname "$0")"
 
 ls -al /
-ls -al /bin/
-ls -al /usr/local/
-ls -al /usr/local/heroku/
-ls -al /usr/local/heroku/bin
+ls -al /app/
 go build -o ${my_dir}/../bin/go-swap-server cmd/server/*.go
