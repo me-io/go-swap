caf41dacc08cc8d01bf58d2dcf7b457503ee1827
Mohamed Meabed <mo.meabed@gmail.com>
2018-09-26 23:56:04 +0400

    Update test

--- cmd/server/convert_test.go
+++ cmd/server/convert_test.go
@@ -48,6 +48,16 @@ func TestConvertObj_Convert(t *testing.T) {
   ],
   "from": "USD",
   "to": "AED"
+}`, `{
+  "amount": 5.5,
+  "exchanger": [
+    {
+      "name": "yahoo",
+      "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0"
+    }
+  ],
+  "from": "USD",
+  "to": "AED"
 }`,
 		`{
   "amount": 4.5,
@@ -76,10 +86,17 @@ func TestConvertObj_Convert(t *testing.T) {
 }`,
 	}
 
-	expected := map[int]string{
+	expectedName := map[int]string{
 		0: "google",
 		1: "yahoo",
-		2: "google",
+		2: "yahoo",
+		3: "google",
+	}
+	expectedRateCache := map[int]string{
+		0: "false",
+		1: "false",
+		2: "true",
+		3: "false",
 	}
 
 	for k, payload := range payloadArr {
@@ -93,6 +110,7 @@ func TestConvertObj_Convert(t *testing.T) {
 		r.Body = ioutil.NopCloser(bytePayloadReader)
 
 		Convert(w, r)
-		assert.Contains(t, testResponse, expected[k])
+		assert.Contains(t, testResponse, expectedName[k])
+		assert.Contains(t, testResponse, expectedRateCache[k])
 	}
 }
